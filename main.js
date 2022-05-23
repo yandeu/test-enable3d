07348d46f965aed7f561c7b509e907bd1db4ffe9
yandeu <20306025+yandeu@users.noreply.github.com>
2022-05-23 18:00:44 +0200

    Make demo scene

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 import { Project, Scene3D, PhysicsLoader } from 'enable3d'
+
+class MainScene extends Scene3D {
+  constructor() {
+    super('MainScene')
+  }
+
+  create() {
+    this.warpSpeed()
+    this.box = this.add.box({ y: 2 }, { lambert: { color: 'deepskyblue' } })
+  }
+
+  update() {
+    this.box.rotation.x += 0.01
+    this.box.rotation.y += 0.01
+  }
+}
+
+new Project({ scenes: [MainScene] })
